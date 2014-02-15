4b8f9e90ce416a8995d5215a65b479c1a890ebf1
Wai Wong <wwong@terabithia.cs.hmc.edu>
2014-02-15 13:30:04 -0800

    Adding blank Xcode project

---
+++ FTIApp/FTIApp/FTIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTIAppDelegate.h
+//  FTIApp
+//
+//  Created by Wai Wong on 2/15/14.
+//  Copyright (c) 2014 FTI Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FTIApp/FTIApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FTIApp
+//
+//  Created by Wai Wong on 2/15/14.
+//  Copyright (c) 2014 FTI Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTIAppDelegate class]));
+    }
+}
